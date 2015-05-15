ab63d28dff3d21c7deb42223da495abc472ec1a6
Cihat <cgenc@schubergphilis.com>
2015-05-15 16:18:41 +0200

    Initial console and service projects added

---
+++ ConsoleClient/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ConsoleClient.Startup))]
+
+namespace ConsoleClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            //  Enable attribute based routing
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
